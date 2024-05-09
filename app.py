20db3ecff67f9ad6efa7ed7dc5b89f0257aa1005
Joonsoo (Kevin) Kim <37880418+snowjs0212@users.noreply.github.com>
2024-05-09 06:48:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 Created on Tue May  7 12:37:55 2024
 
 @author: Joonsoo
+Reference: https://github.com/andymcdgeo/streamlit_tutorial_series
 """
 #### Packages
 import streamlit as st
@@ -13,16 +14,18 @@ st.title("Joonsoo's first test on streamlit")
 st.text("This is a testing environment to build a better analytics tool in the future.")
 st.markdown("**Gayun** is my wife, and she is cute(?).")
 
-#### File import
-#uploaded_file = st.file_uploader("Upload your file here.")
-#
-#if uploaded_file:
-#    df = pd.read_csv(uploaded_file)
-#    st.write(df.describe())
-
+#### Imunika coffee data
 st.header("LookUp - Imunika coffee research data")
-df = pd.read_csv("Imunika_Pilot_Stage1_Final.csv")
-st.write(df.describe())
+df_1 = pd.read_csv("Imunika_Pilot_Stage1_Final.csv")
+st.write(df_1.describe())
 
 st.header("Data header summary")
-st.write(df.head(10))
+st.write(df_1.head(10))
+
+#### File import
+st.header("User data analytics tool (csv file supported)")
+uploaded_file = st.file_uploader("Upload your file here.")
+
+if uploaded_file:
+    df_2 = pd.read_csv(uploaded_file)
+    st.write(df_2.describe())
