af869d9622399857c248a5f750bbb202c7b787c8
Joonsoo (Kevin) Kim <37880418+snowjs0212@users.noreply.github.com>
2024-05-21 14:07:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,6 @@ if uploaded_file:
     df_2 = pd.read_csv(uploaded_file)
     st.write(df_2.describe())
 
-#### API key
-fred_api_key = "7a3a846e27994737590201d1ed80f0f1"
-
 #### Get data
 fred = Fred(api_key = fred_api_key)
 
